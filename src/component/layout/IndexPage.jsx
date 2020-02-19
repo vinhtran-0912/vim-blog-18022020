db2c4240f9d40bab7f999586d450ed96471bacea
vinhtran-0912 <vinhtran09121997@gmail.com>
2020-02-19 17:05:44 +0700

    add pattern for layout and css, route

--- src/component/layout/IndexPage.jsx
+++ src/component/layout/IndexPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header';
 import '../../styles/layout/indexPage.css';
 
-const IndexPage = () => (
-    <div className="container">
-        <Header/>
-    </div>
-);
+const IndexPage = () => {
+  return (
+    <Router>
+      <div className="container">
+        <Header />
+      </div>
+    </Router>
+  );
+};
 
 export default IndexPage;
