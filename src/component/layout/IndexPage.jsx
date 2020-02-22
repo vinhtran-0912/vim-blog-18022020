d6f860e47a97ab9c1c7837b914d38335573388a6
vinhtran-0912 <vinhtran09121997@gmail.com>
2020-02-22 12:56:53 +0700

    add context

--- src/component/layout/IndexPage.jsx
+++ src/component/layout/IndexPage.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header';
+
+import { ThemeContext } from '../../context/ThemeContext';
 import '../../styles/layout/indexPage.css';
+
+const AppContext = React.createContext();
 
 const IndexPage = () => {
   return (
@@ -10,6 +14,12 @@ const IndexPage = () => {
       <div className="container">
         <Header />
       </div>
+      <ThemeContext.Provider value={this.state.theme}>
+        {/* <Toolbar changeTheme={this.toggleTheme} /> */}
+      </ThemeContext.Provider>
+      <Section>
+        <ThemedButton />
+      </Section>
     </Router>
   );
 };
